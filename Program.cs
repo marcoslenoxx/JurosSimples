e06185c7ac1f7eaed29b13f62578db70e1d19fd7
marcoslenoxx <marcos.lenoxx84@gmail.com>
2025-04-04 20:42:19 -0300

    Finalizado

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿﻿decimal capital, taxa, meses, juros, montante;
+
+Console.WriteLine("Juros simples (j)\n");
+Console.ForegroundColor = ConsoleColor.Cyan;
+
+Console.Write("Capital [c] (R$).......: ");
+Console.ResetColor();
+capital = Convert.ToDecimal(Console.ReadLine());
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+Console.Write("Taxa de juros [i] (%)..: ");
+Console.ResetColor();
+taxa = Convert.ToDecimal(Console.ReadLine());
+Console.ForegroundColor =ConsoleColor.Yellow;
+Console.Write("Tempo [t] (meses)......: ");
+Console.ResetColor();
+meses = Convert.ToDecimal(Console.ReadLine());
+
+juros = capital * (taxa / 100) * meses;
+montante = capital + juros;
+
+Console.WriteLine($"\nJuros.....: {juros:C2}");
+Console.WriteLine($"Montante..: {montante:C2}");
